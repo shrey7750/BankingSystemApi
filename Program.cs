14494d6b1aa0ab7bfabbefa448665dc9b7e9bb36
shrey7750 <shrey7750@gmail.com>
2024-05-24 17:37:53 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace BankSystemApi
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => options.AddServerHeader = false)
+                .UseStartup<Startup>();
+    }
+}
