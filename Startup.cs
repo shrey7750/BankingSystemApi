1a3cda2869dba00567c337f40f5974d4554d555f
shrey7750 <shrey7750@gmail.com>
2024-05-24 17:39:01 +0530

    Create Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<List<User>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Add error handling middleware
+            app.UseErrorHandlingMiddleware();
+        }
+    }
